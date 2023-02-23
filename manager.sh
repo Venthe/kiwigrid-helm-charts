8a527132694de808dc6ecad64760397e80207455
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2023-02-23 11:38:58 +0100

    Add variables to manager.sh

--- manager.sh
+++ manager.sh
@@ -2,18 +2,31 @@
 
 set -o errexit
 set -o pipefail
+set -o xtrace
+
+. .env 2>/dev/null 1>/dev/null || true
+
+DIST_DIRECTORY=dist
+CONTEXT_PATH="${CONTEXT_PATH:-repository/helm-hosted/}"
+REPOSTORY="${REPOSITORY:-https://nexus.home.arpa/${CONTEXT_PATH}}"
+REPOSITORY_USERNAME="${REPOSITORY_USERNAME:-admin}"
+REPOSITORY_SECRET="${REPOSITORY_SECRET:-secret}"
+TARGET_REPOSITORY_NAME="${TARGET_REPOSITORY_NAME:-hosted}"
 
 function deploy() {
-    mkdir -p dist && helm package -d dist ./chart
+    local chart_name="${1}"
+    local chart_archive=`cat charts/${chart_name}/Chart.yaml | yq -e '(.name + "-" + .version + ".tgz")'`
+
+    mkdir -p "${DIST_DIRECTORY}" && helm package -d "${DIST_DIRECTORY}" "./charts/${chart_name}"
 
     helm plugin install https://github.com/chartmuseum/helm-push || true
-    helm repo add hosted https://nexus.home.arpa/repository/helm-hosted/ || true
+    helm repo add "${TARGET_REPOSITORY_NAME}" "${REPOSTORY}" || true
     helm cm-push \
-    --username=admin \
-    --password=secret \
-    ./dist/fluentd-opensearch-13.9.1.tgz \
-    --context-path=/repository/helm-hosted/ \
-    hosted
+      --username="${REPOSITORY_USERNAME}" \
+      --password="${REPOSITORY_SECRET}" \
+      "./${DIST_DIRECTORY}/${chart_archive}" \
+      --context-path="/${CONTEXT_PATH}" \
+      "${TARGET_REPOSITORY_NAME}"
 }
 
 function release_local() {
@@ -31,4 +44,5 @@ function logs() {
     kubectl logs --namespace infrastructure ds/fluentd-infrastructure-fluentd-opensearch --follow
 }
 
+# bash ./manager.sh deploy fluentd-opensearch
 ${@}
